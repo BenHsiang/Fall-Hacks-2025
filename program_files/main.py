e5893f6ae00149874d28c871ace552ed99929e4b
BenHsiang <bhsiang0219@gmail.com>
2025-09-27 10:51:54 -0700

    Fixed checklist formatting, restructured project directory

---
+++ program_files/main.py
@@ -0,0 +1,19 @@
+# Fall Hacks 2025 Project
+# 
+# main.py
+# 
+# Benley Hsiang
+# Aeris Chan
+# Brandon Fong
+# Duc anh Nguyen
+#
+
+import test
+
+def main():
+
+    test.test()
+
+    
+if __name__ == "__main__":
+    main()
---
+++ program_files/test.py
@@ -0,0 +1,4 @@
+
+def test():
+    print("hello")
+    return 0
