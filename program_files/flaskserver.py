346607fe12017a36e7ec0395c41fa0fc3e5f8a89
Aeoli11 <writeaeris@gmail.com>
2025-09-27 16:28:23 -0700

    Update flaskserver.py

--- program_files/flaskserver.py
+++ program_files/flaskserver.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import ClassCart
 import ClassDish
-
+import json
 # Sets up flask
 app = Flask(__name__)
 # No idea wtf this does but its very important
@@ -22,11 +22,12 @@ def receive_json():
         #moving data from data into cart class
         #assuming calls for data are like this though I wouldnt know. 
         #prays in will run
+       # dataloaded=json.loads(data)
        # MyCart=ClassCart.Cart(1111,"User")
-      #  for x in data:
-       #     Dish=ClassDish.Dish(x.price,0,x.title,x.restaurant)
-       #     MyCart.addDishToCart(Dish,1)
-      #  MyCart.PrintOrder
+        #for x in dataloaded:
+        #   Dish=ClassDish.Dish(x.price,0,x.title,x.restaurant)
+        #   MyCart.addDishToCart(Dish,1)
+        #MyCart.PrintOrder
 
     
     # Not important, just error messages 
